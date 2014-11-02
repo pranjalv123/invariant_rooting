9be8eb19bae16eb42aa012c78592105bb5fec3e3
Ruth Davidson <redavids@ncsu.edu>
2014-11-02 17:43:21 -0600

    added files for set matrix using binary set representation

---
+++ matrixmaker.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class MatrixMaker:
+    def __init__(self, labels, setlist):
+        self.labels = labels
+        self.setlist = setlist
+        self.binlabels = {}
+        for i in range(len(labels)):
+            self.binlabels[labels[i]] = 2**i
+
+    def binset(self, setlistentry):
+        b = 0
+        for j in range(len(setlistentry)):
+            b = b + self.binlabels[setlistentry[j]]
+        return b
+       
+    def inclusion(self, set1, set2):
+        mask1 = self.binset(set1)
+        mask2 = self.binset(set2)
+        return mask1 == (mask1 & mask2)
+
+    def matrix(self):
+        #M = np.zeros((len(self.setlist), len(self.setlist)))
+        K = []
+        for i in range(len(self.setlist)):
+            row = [self.inclusion(self.setlist[j], self.setlist[i]) for j in range(len(self.setlist))]
+            K.append(row)
+        M = np.array(K)
+        return M
+   
+        
---
+++ tests/test_matrixmaker.py
@@ -0,0 +1,44 @@
+import unittest
+import matrixmaker
+
+class TestmatrixMaker(unittest.TestCase):
+    def test_binlabels(self):
+        labels = ['MOM', 'DAD', 'BOB']
+        setlist = [['MOM', 'BOB'], ['DAD']]
+        matrix_maker = matrixmaker.MatrixMaker(labels, setlist)
+        bin1 = matrix_maker.binlabels["MOM"]
+        bin2 = matrix_maker.binlabels['BOB']
+        self.assertEqual(bin1, 2**0)
+        self.assertEqual(bin2, 2**2)
+       
+
+    def test_setlistlabels(self):
+        labels = ['MOM', 'DAD', 'BOB']
+        setlist = [['MOM', 'BOB'], ['DAD']]
+        matrix_maker = matrixmaker.MatrixMaker(labels, setlist)
+        setbin1 = matrix_maker.binset(['MOM', 'BOB'])
+        self.assertEqual(setbin1, 2**0 + 2**2)
+
+    def test_containment(self):
+        labels = ['MOM', 'DAD', 'BOB']
+        setlist = [['MOM', 'BOB'], ['DAD'],['MOM', 'DAD', 'BOB']]
+        matrix_maker = matrixmaker.MatrixMaker(labels, setlist)        
+        setbin1 = matrix_maker.binset(['MOM', 'BOB'])
+        setbin2 = matrix_maker.binset(['MOM', 'DAD', 'BOB'])
+        setbin3 = matrix_maker.binset(['DAD'])
+        test1 = matrix_maker.inclusion(['MOM', 'BOB'], ['MOM', 'DAD', 'BOB'])
+        test2 = matrix_maker.inclusion(['DAD'], ['MOM', 'BOB'])        
+        self.assertEqual(test1, 1)
+        self.assertEqual(test2, 0)
+
+    def test_make_matrix(self):
+        labels = ['MOM', 'DAD', 'BOB']
+        setlist = [['MOM', 'BOB'], ['DAD'],['MOM', 'DAD', 'BOB']]
+        matrix_maker = matrixmaker.MatrixMaker(labels, setlist)        
+        M = matrix_maker.matrix()
+        self.assertEqual(M.shape, (3,3))
+        self.assertEqual(M[2,0], 1)
+        self.assertEqual(M[1,0], 0)
+  
+        
+    
